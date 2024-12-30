7c76c6fd27a6613c586c99603e6461ce3e017c54
NOGiTTiS <Knight_Darkwing@hotmail.com>
2024-12-30 23:26:45 +0700

    day18

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\PayController;
 use App\Http\Controllers\RoomController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -16,3 +17,4 @@ Route::get('/company/index', [CompanyController::class, 'index']);
 Route::get('/room', [RoomController::class, 'index']);
 Route::get('/customer', [CustomerController::class, 'index']);
 Route::get('/pay', [PayController::class, 'index']);
+Route::get('/user', [UserController::class, 'index']);
